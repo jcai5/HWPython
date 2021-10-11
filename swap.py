e293e2e606af65e08e1fde2a6b4224fe06e4e116
James Cai <james.y.cai@sjsu.edu>
2021-10-10 19:20:14 -0700

    First commit adding completed swap last item function

---
+++ swap.py
@@ -0,0 +1,16 @@
+def swap_last_item(myList)
+	"""
+	Swaps the last element in a list with the first element.
+	
+	Parameters
+	----------
+	myList:list of integers
+	Returns
+	-------
+	list: myList with first and last element swapped
+
+	"""
+	temp = myList[-1]
+	myList[-1] = myList[0]
+	myList[0] = temp
+	return myList
