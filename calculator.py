66f3f607af56c572b94860b29cfb230f4033c743
James Cai <james.y.cai@sjsu.edu>
2021-10-10 20:38:00 -0700

    removed parse_input call and added doc strings for both functions

--- calculator.py
+++ calculator.py
@@ -1,4 +1,17 @@
 def calculator(number1, number2, operator):
+	"""
+	calculator function computes +, -, *, /, //, and ** using the inputs number1, number2, and the operator.
+	Parameters
+	----------
+	number1 : float
+	number2 : float
+	operator : string
+
+	Returns
+	-------
+	float depending on the operator or false if the operator is not found
+	
+	"""
 	if operator == '+':
 		return number1+number2
 	if operator == '-':
@@ -13,7 +26,11 @@ def calculator(number1, number2, operator):
 		return number1**number2
 	else:
 		return False
+
 def parse_input():
+	"""
+	parse_input takes the input of the user, parses the input and passes it to the calculator.
+	"""
 	str = (raw_input("Enter equation: "))
 	x = str.split()
 	num1 = float(x[0])
@@ -21,4 +38,3 @@ def parse_input():
 	op = (x[1])
 	print (calculator(num1, num2, op))
 
-parse_input()
